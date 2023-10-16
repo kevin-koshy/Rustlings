e6a21b50cc24d5f01685885e0ef9b7c0a7a8e8a6
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-16 01:09:33 -0400

    Initial Commit

---
+++ intro/intro2.rs
@@ -0,0 +1,13 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+fn main() {
+    let greeting = "world".to_string();
+    println!("Hello {}!", greeting);
+}
