75808cd139d02ec77861d2adb3c49aec82468f32
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-22 00:18:09 -0400

    clippy1/2/3.rs

--- clippy/target/debug/deps/clippy1-5662668f8ecf6537.d
+++
@@ -1,9 +0,0 @@
-/Users/kevinkoshy/Documents/Development/Rust/Projects/Rustlings/exercises/clippy/target/debug/deps/clippy1-5662668f8ecf6537.rmeta: clippy1.rs Cargo.toml
-
-/Users/kevinkoshy/Documents/Development/Rust/Projects/Rustlings/exercises/clippy/target/debug/deps/clippy1-5662668f8ecf6537.d: clippy1.rs Cargo.toml
-
-clippy1.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
